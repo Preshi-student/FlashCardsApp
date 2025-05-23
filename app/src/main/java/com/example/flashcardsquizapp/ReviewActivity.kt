26e79faf9c1df4ec3ab65002ed29669b3565abf1
Precious <ST10470248@vcconnect.edu.za>
2025-05-23 23:39:01 +0200

    Update all project files for latest changes

--- app/src/main/java/com/example/flashcardsquizapp/ReviewActivity.kt
+++ app/src/main/java/com/example/flashcardsquizapp/ReviewActivity.kt
@@ -1,18 +1,17 @@
 package com.example.flashcardsquizapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.enableEdgeToEdge
+import kotlin.system.exitProcess
 
 class ReviewActivity : AppCompatActivity() {
     private lateinit var Review: TextView
     private lateinit var Restart: Button
     private lateinit var Exit: Button
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,19 +24,30 @@ class ReviewActivity : AppCompatActivity() {
         Exit = findViewById(R.id.Exit)
 
         // Fetch questions and answers from the Scorescreen
-        val questions = intent.getStringExtra("questions") // Array of the questions
-        val answers = intent.getBooleanArrayExtra("answers") // Array of boolean answers true/false
+        val questions = intent.getStringArrayExtra("questions") // Array of the questions
+        val answers = intent.getBooleanArrayExtra("answers")    // Array of boolean answers true/false
 
-        val Review = StringBuilder()
-        if (questions != null && questions != null&& answers.size == answers.size ) {
+        if (questions != null && answers != null && questions.size == answers.size) {
+            val reviewBuilder = StringBuilder()
             for (i in questions.indices) {
-                Review.append("${i+1}. ${questions[i]}\n")
-                Review.append("  Answer: ${if  ( answers[i])     "True"  else "False"  }\n\n ")
-
-
-
+                // Parallel arrays of questions and answers
+                reviewBuilder.append("${i + 1}. ${questions[i]}\n")
+                reviewBuilder.append("  Answer: ${if (answers[i]) "True" else "False"}\n\n")
             }
+            Review.text = reviewBuilder.toString()
+        }else{
+            Review.text = "There was an error that occured when trying to load the review data"
 
-
+        }
+        // Set restart button on click listener
+        Restart.setOnClickListener{
+            // Link to the previous Flashquestions screen
+            val intent = Intent(this,FlashQuestionActivity::class.java)
+            startActivity(intent)
+        }
+        Exit.setOnClickListener{
+            finish()
+            exitProcess(0)   // Close the app
+        }
     }
 }
